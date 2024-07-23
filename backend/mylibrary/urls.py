a6842195e674d8c43f254b4e8c28ffb2d5e213f0
rohandhother <rohandhother@gmail.com>
2024-07-23 22:29:21 +0100

    fix: admin settings fixed and reinstated

--- backend/mylibrary/urls.py
+++ backend/mylibrary/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
 router.register('books', views.BookViewSet)
-router.register('readers', views.ReaderViewSet)
+router.register('readers', views.ReaderViewSet, basename='readers')
+router.register('reader_names',
+                views.ReaderNameViewSet,
+                basename='reader_names')
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls))
 ]
