77c646d04d2c387589c126734fbfc08e0c05ef53
rohandhother <rohandhother@gmail.com>
2024-07-23 17:23:38 +0100

    feat: books displays reader's name

--- backend/mylibrary/serializer.py
+++ backend/mylibrary/serializer.py
@@ -3,13 +3,16 @@ from .models import Book, Reader
 
 
 class BookSerializer(serializers.ModelSerializer):
+    taken_by = serializers.SlugRelatedField(
+        queryset=Reader.objects.all(),
+        slug_field='name'
+    )
     class Meta:
         model = Book
         fields = '__all__'
 
 
 class ReaderSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Reader
         fields = '__all__'
